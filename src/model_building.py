e1ccd7006f91be1e61be3a7c724921233e522902
Vaibhav <vaibhavlokhande1798@gmail.com>
2026-04-06 20:13:58 +0530

    params configure with DVC, With params.yaml

--- src/model_building.py
+++ src/model_building.py
@@ -4,6 +4,7 @@ import pandas as pd
 import pickle
 import logging
 from sklearn.ensemble import RandomForestClassifier
+import yaml
 
 # Ensure the "logs" directory exists
 log_dir = "logs"
@@ -32,22 +33,22 @@ console_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 logger.addHandler(console_handler)
 
-# def load_params(params_path: str) -> dict:
-#     """Load parameters from a YAML file."""
-#     try:
-#         with open(params_path, 'r') as file:
-#             params = yaml.safe_load(file)
-#         logger.debug('Parameters retrived successfully from: %s', params_path)
-#         return params
-#     except FileNotFoundError:
-#         logger.error('Parameters file not found: %s', params_path)
-#         raise
-#     except yaml.YAMLError as e:
-#         logger.error('Error parsing YAML file: %s', e)
-#         raise
-#     except Exception as e:
-#         logger.error('Unexpected error while loading the parameters: %s', e)
-#         raise
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrived successfully from: %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('Parameters file not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('Error parsing YAML file: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error while loading the parameters: %s', e)
+        raise
 
 def load_data(file_path: str) -> pd.DataFrame:
     """Load data from a CSV file """
@@ -113,7 +114,8 @@ def save_model(model, file_path: str) -> None:
 
 def main():
     try:
-        params = {'n_estimators' : 25, 'random_state' : 2}
+        #params = {'n_estimators' : 25, 'random_state' : 2}
+        params = load_params('params.yaml')['model_building']
         train_data = load_data('./data/processed/train_tfidf.csv')
         x_train = train_data.iloc[:, :-1].values
         y_train = train_data.iloc[:, -1]. values
